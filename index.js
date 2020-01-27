561a49135f1c1ed53a3a5a2c7859033d0163ccb8
ankita <ksankita16@gmail.com>
2020-01-27 23:16:00 +0530

    abc

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope) {
-  $scope.firstName = "John";
-  $scope.lastName = "Doe";
+app.controller('myCtrl', function($scope,$http) {
+
+
+$scope.cityList = ['MUMBAI','BANGALORE','DELHI','PUNE','CHENNAI']
+
+$scope.data = {
+  selectCity:$scope.cityList[0]
+}
+
+$scope.fetchData = function(){
+  $http({
+  method: 'GET',
+  url: 'https://vast-shore-74260.herokuapp.com/banks?city='+$scope.data.selectCity
+}).then(function successCallback(response) {
+  $scope.bankList = response.data
+  }, function errorCallback(response) {
+  });
+}
+$scope.fetchData()
+
 });
