aea392334d556bc690c0f93477c05070034013c8
ankita <ksankita16@gmail.com>
2020-01-27 23:40:36 +0530

    abc

--- index.js
+++ index.js
@@ -5,13 +5,21 @@ app.controller('myCtrl', function($scope,$http) {
 $scope.cityList = ['MUMBAI','BANGALORE','DELHI','PUNE','CHENNAI']
 
 $scope.data = {
-  selectCity:$scope.cityList[0]
+  selectCity:$scope.cityList[0],
+  searchText :''
 }
-
+$scope.data.searchText =''
 $scope.fetchData = function(){
+  var url = 'https://vast-shore-74260.herokuapp.com/banks'
+  if ($scope.data.searchText.length>0) {
+    url = '?city__icontains='+$scope.data.searchText
+  }
+  else{
+    url += '?city='+$scope.data.selectCity
+  }
   $http({
   method: 'GET',
-  url: 'https://vast-shore-74260.herokuapp.com/banks?city='+$scope.data.selectCity
+  url: url
 }).then(function successCallback(response) {
   $scope.bankList = response.data
   }, function errorCallback(response) {
